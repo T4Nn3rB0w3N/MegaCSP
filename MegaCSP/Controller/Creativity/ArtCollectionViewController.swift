19408581f976c710cbb403e3ebebbeb767bb0581
Bowen <tbow1382@740s-pl-120.local>
2019-01-04 09:53:03 -0700

    Initial commit
    gitignored

---
+++ MegaCSP/Controller/Creativity/ArtCollectionViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ArtCollectionViewController.swift
+//  CreativityProject
+//
+//  Created by Bowen, Tanner on 11/7/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "artIdentifier"
+
+public class ArtCollectionViewController: UICollectionViewController
+{
+    
+    //MARK: Data members for Creativity Screen
+    
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRowCompact : CGFloat = 4
+    private let itemsPerRowNormal : CGFloat = 6
+    
+    private let creativeCS : [UIImage?] =
+    {
+        return [
+            UIImage(named: "javaHaiku"),
+            UIImage(named: "mainframeHaiku"),
+            UIImage(named: "octocat"),
+            UIImage(named: "swiftHaiku")
+        ]
+    }()
+    
+    //MARK: - Lifecycle
+    
+    private let labels : [String] =
+    {
+        return [
+            "My java haiku",
+            "My mainframe haiku",
+            "My octokitty",
+            "My swift Haiku"
+        ]
+    }()
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    public override func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+
+
+    public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return creativeCS.count
+    }
+
+    public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let artCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCell
+    
+        artCell.backgroundColor = .purple
+        artCell.artImage.image = creativeCS[indexPath.row]
+        artCell.artLabel.text = labels[indexPath.row]
+    
+        return artCell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRowCompact
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    public override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        let imageView = UIImageView(image: creativeCS[indexPath.row])
+        imageView.frame = self.view.frame
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        imageView.addGestureRecognizer(tap)
+        
+        self.view.addSubview(imageView)
+    }
+    
+    //use to go back from full mode
+    @objc
+    private func dismissFullscreenImage(_ sender: UITapGestureRecognizer)
+    {
+        sender.view?.removeFromSuperview()
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return sectionInsets
+    }
+    
+    public func collectionview(_ collectionview: UICollectionViewLayout,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+        return sectionInsets.left
+    }
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+ override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool
+ {
+    return true
+ }
+ */
+    
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+ override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool
+ {
+ return false
+ {
+ 
+ override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexpath: IndexPath, withSender sender: Any?) -> Bool
+ {
+ return false
+ }
+ 
+ override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?)
+ {
+ }
+ */
+    
+}
+
+   
+
+
+
---
+++ MegaCSP/Controller/Creativity/CreativityViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreativityViewController.swift
+//  CreativityProject
+//
+//  Created by Bowen, Tanner on 11/1/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import UIKit
+
+public class CreativityViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   public override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
